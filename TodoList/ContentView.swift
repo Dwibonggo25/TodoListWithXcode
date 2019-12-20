3ba36dc5940b82cc4360defd8e6148b3ec25f778
Dwi Bonggo <dwibonggo25@gmail.com>
2019-12-20 16:27:23 +0700

    First Commit

---
+++ TodoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by Dwi Bonggo Pribadi on 12/20/19.
+//  Copyright © 2019 Dwi Bonggo Pribadi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
